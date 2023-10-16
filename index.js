091b131cef6943b2d208929ed22ca5c0462a76d9
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-16 21:06:20 +0600

    56-2 Client Side router and Connect MongoDB database

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5001;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  //   "mongodb+srv://<username>:<password>@cluster0.nmv0r0r.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmv0r0r.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Coffee Store server is running");
