82827304faa3e9e1a000a23197ba663855450949
ronee-muhammad-rayhan <muhammad@rayhanworld.com>
2023-10-17 09:20:51 +0600

    56-3 Server Side hide database User and Password using Dotenv config

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +40,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
