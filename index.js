c2dd31c895f04302d3ea61a06a4ee6cd45a0abe5
ronee-muhammad-rayhan <muhammad@rayhanworld.com>
2023-10-17 17:23:15 +0600

    56-8 Update single coffee information

--- index.js
+++ index.js
@@ -55,6 +55,27 @@ async function run() {
 
     })
 
+    app.put('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) } //filter=query
+      const options = { upsert: true }
+      const updatedCoffee = req.body
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result)
+
+    })
+
     app.delete('/coffees/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
